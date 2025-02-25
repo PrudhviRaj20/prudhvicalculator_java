3b1b66b9fe4c8e3305f8402556947a3272a9fe35
PrudhviRaj20 <144943708+PrudhviRaj20@users.noreply.github.com>
2025-02-25 23:09:49 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,10 @@
+
+        function calculateResult() {
+            try {
+                document.getElementById('expression').value = eval(document.getElementById('expression').value);
+            } catch (e) {
+                alert('Invalid Expression');
+                document.getElementById('expression').value = '';
+            }
+        }
+        
